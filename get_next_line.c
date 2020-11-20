47f01d372f548fd1c762dd8a08ca43b8b787ca57
rvan-duy <rvan-duy@student.codam.nl>
2020-11-20 19:52:04 +0100

    more initial commits

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,19 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/20 18:43:46 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2020/11/20 18:47:39 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2020/11/20 19:51:10 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-**	Calling this function will read the text available
-**	on the file descriptor, one line at a time until
-**	the end of the file
+**	get_next_line returns a line read from a file descriptor, without the newline.
 */
+
+#include "get_next_line.h"
 
 int	get_next_line(int fd, char **line)
 {
 	
+	
+	read(fd, 0, 1000);
 }
---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/20 19:40:58 by rvan-duy      #+#    #+#                 */
+/*   Updated: 2020/11/20 19:44:21 by rvan-duy      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+int get_next_line(int fd, char **Line);
+
+#endif
