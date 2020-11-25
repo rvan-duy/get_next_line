e92e7161fa31d2c143a04c40e776ffd0aa37e924
rvan-duy <rvan-duy@student.codam.nl>
2020-11-25 17:01:35 +0100

    fixes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/20 18:43:46 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2020/11/25 15:50:57 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2020/11/25 17:01:11 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,20 +35,19 @@ static int find_nline(char *line)
 	return (-1);
 }
 
-static char *cut_until_nline(char **line)
+static char *cut_until_nline(char *line)
 {
 	int i;
 	char *newstr;
 
-	newstr = malloc(sizeof(char) * find_nline(*line) + 1);
-
+	newstr = malloc(sizeof(char) * find_nline(line) + 1);
 	i = 0;
-	while (i < i)
+	while (line[i] != '\0' && line[i] != '\n')
 	{
-		printf("%d ", i);
 		newstr[i] = line[i];
 		i++;
-	}	
+	}
+	return (newstr);
 }
 
 int	get_next_line(int fd, char **line)
@@ -56,19 +55,23 @@ int	get_next_line(int fd, char **line)
 	*line = calloc(BUFFER_SIZE, sizeof(char));
 	int ret = read(fd, *line, BUFFER_SIZE);
 	char *newstr;
-	newstr = calloc(1000, sizeof(char));
 	// Buffer_size has been read
 	// 		when a newline is found
 	//		when the end of the file is foun
-	/*if (newline != -1)
+	
+	
+	if (ret != -1)
 	{
-		printf("Newline found on index: %d\n", newline);
-		printf("[%s]", *line);
-		newstr = cut_until_nline(*line, newline);
-	}*/
-	printf("[%p]\n", line);
-	printf("[%s]\n", line[0]);
-	printf("[%c]\n", line[0][1]);
+		printf("[%s]\n", *line);
+		newstr = cut_until_nline(*line);
+		printf("[%s]\n", newstr);
+	}
+	
+	
+	
+	//printf("[%p]\n", line);
+	//printf("[%s]\n", line[0]);
+	//printf("[%c]\n", line[0][1]);
 
 
 	
