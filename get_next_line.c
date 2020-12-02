379318161f559f24b1df33c867ef531478f26ead
rvan-duy <rvan-duy@student.codam.nl>
2020-12-02 21:49:14 +0100

    added len check

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/02 14:26:58 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2020/12/02 19:55:52 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2020/12/02 21:47:57 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,23 @@ int get_next_line(int fd, char **line)
     int ret;
     int newline;
     int end;
+    int strlen;
 
     end = 0;
     newline = 0;
     while (newline == 0)
     {
-        ret = read(fd, buf, BUFFER_SIZE);
+        strlen = gnl_strlen(buf);
+        ret = read(fd, buf + strlen, BUFFER_SIZE - strlen);
         if (ret > 0)
         {
             newline = gnl_find_nline(buf);
             // This needs error handeling but i have no idea how
             line[0] = gnl_strjoin(buf, line, newline);
+            
             printf("[%s]\n", line[0]);
         }
+        return 0;
         if (ret == -1)
             return (-1);
         if (end == 1)
