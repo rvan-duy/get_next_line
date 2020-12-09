b0569c051855c791caa6c4caa7d6e9c7d188a4b7
ruben <rvan-duy@codam.nl>
2020-12-09 15:41:53 +0100

    tests

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/02 14:26:58 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2020/12/09 15:30:27 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2020/12/09 15:32:02 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,46 +48,5 @@ int			get_next_line(int fd, char **line)
 	if (!line || fd < 0)
 		return (-1);
 	ret = gnl_make_line(buf, line, fd, ret);
-	if (ret == -1)
-		return (-1);
-
-
-
-
-
-
-
-
-	/*newline = 0;
-    while (newline == 0)
-    {
-		if (buf[0])
-		{
-			newline = gnl_find_nline(buf);
-			line[0] = gnl_strjoin(buf, line, newline);
-			if (line[0] == NULL)
-				return (-1);
-			gnl_parsebuffer(buf, newline);
-		}
-		else
-		{
-			ret = read(fd, buf, BUFFER_SIZE);
-			printf("ret: [%d]\n", ret);
-			if (ret == 0)
-				return (0);
-			///////////// what to when read returns 0 or -1?
-			buf[ret + 1] = '\0';
-			printf("buf: [%s]\n", buf);
-			newline = gnl_find_nline(buf);
-			printf("newline: [%d]\n", newline);
-			line[0] = gnl_strjoin(buf, line, newline);
-			printf("after joining line: [%s] || buf: [%s]\n", line[0], buf);
-			///////////// this strjoin needs error handling, also free stuff but how?
-			if (line[0] == NULL)
-				return (-1);
-			gnl_parsebuffer(buf, newline);
-			printf("after parsing: line: [%s] || buf: [%s]\n", line[0], buf);
-		}
-    }*/
     return (ret);
 }
